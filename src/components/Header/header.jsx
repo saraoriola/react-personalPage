6b13bb8843177ed783da46677abda09a6debc7d8
saraoriola <saraoriola@outlook.com>
2023-07-26 08:11:45 +0200

    arreglando div

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,20 +3,20 @@ import Logo from '../../assets/sara.png';
 import "./header.css";
 
 const Header = () => {
-  return (
-    <header>
-      <nav>
-      <div className="logo">
+  return ( 
+    <div className="header-container">
+      <header> 
+        <nav>
           <img src={Logo} alt="Logo" />
+            <ul className="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
       </div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
   );
 };
 
